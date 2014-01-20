cbdcbd08845d1a265762636c65efabe09e520184
RIVOMANANA <rivomanana.android@gmail.com>
2014-01-20 22:13:30 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '52a297a6fab37366f4cf144b48a876dd91d5dffd1ef89f0cada77894c8f6e365fed098fba23d6cf5a11d814f7203fb9800ec7757210ab36f3e410769f4dcde4d'
